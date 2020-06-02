0b4196108df6dbfcd713e4393759ae4896330f83
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-02 23:54:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,10 @@ def monopoly_with_third_tier
 	    :names => {
 	      :pennsylvania_railroad => {}
 	    },
-	    :rent_in_dollars => {}
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25
+	      :two_pieces_owned => 50
+	    }
 	  }
 	}
 
