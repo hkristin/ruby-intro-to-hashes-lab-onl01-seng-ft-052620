38a653a504fd1c7144134fd299ca05d5993e73b1
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-02 23:41:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 def monopoly_with_second_tier
 		monopoly = {
 	  :railroads => {
-	    :pieces => "4"
+	    :pieces => "4",
 	    :rent_in_dollars => {
 	      :one_piece_owned => "25",
 	      :two_pieces_owned => "50",
